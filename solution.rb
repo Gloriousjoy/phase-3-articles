0d25309bd3c87d9e746c3356262068d080cb7657
Gloriousjoy <gloriousfab99@gmail.com>
2022-10-28 10:04:24 +0300

    initialize method

--- solution.rb
+++ solution.rb
@@ -1,1 +1,10 @@
 # Please copy/paste all three classes into this file to submit your solution!
+#Author
+class Authors
+    attr_accessor :name 
+    @@all = []
+    def initilize (name)
+        @name = name
+        @@all << self
+    end
+end
